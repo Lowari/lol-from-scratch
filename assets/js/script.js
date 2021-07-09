fc2af7559982f4f624b60ec92a5d037336fc60bb
Lowari <66225181+Lowari@users.noreply.github.com>
2021-07-09 17:45:11 +0200

    Responsive page connexion up

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,31 @@ for (i = 0; i < links.length; i++) {
         this.link.classList.add('currentPage');
     }
 }
+
+// Page connexion 
+
+const mail_connexion = document.getElementById('email_connexion');
+const error_mail_connexion = document.getElementById('error_mail_connexion');
+const password_connexion = document.getElementById('password_connexion');
+const error_password_connexion = document.getElementById('error_password_connexion');
+
+/**
+ * Vérifie une fois que l'input a perdu le focus si le champ est vide
+ * @param {HTMLElemennt} input input target 
+ * @param {HTMLElement} element element où inscrire le message
+ */
+
+function check_empty_input(input, element) {
+    input.addEventListener('focusout', (e) => {
+        if (e.target.value == '') {
+            element.innerText = "le champ est vide";
+            input.classList.add('wrong');
+        } else {
+            element.innerText = "";
+            input.classList.remove('wrong');
+        }
+    })
+}
+
+check_empty_input(mail_connexion, error_mail_connexion);
+check_empty_input(password_connexion, error_password_connexion);
